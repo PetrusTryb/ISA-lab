8d24261ac07bbb5b19fde067e1c6b8da1e485ce2
Piotr Trybisz <ptrybisz@gmail.com>
2024-10-20 00:07:06 +0200

    Enable H2 InMemory database preview in IntelliJ

--- src/main/java/com/trybisz/isa/Main.java
+++ src/main/java/com/trybisz/isa/Main.java
@@ -1,12 +1,21 @@
 package com.trybisz.isa;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(Main.class, args).close();
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
 }
